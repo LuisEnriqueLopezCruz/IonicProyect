fb9b634b3c1544296abed4bf0760f34c6760b3c2
LuisEnriqueLopezCruz <luis.lopezc@axity.com>
2024-04-12 21:11:19 -0600

    PruebasV1

--- src/app/login/login.page.spec.ts
+++ src/app/login/login.page.spec.ts
@@ -1,17 +1,75 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { LoginPage } from './login.page';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthenticationService } from '../services/authentication.service';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { of, throwError } from 'rxjs';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
+  let authServiceSpy: jasmine.SpyObj<AuthenticationService>;
+  let routerSpy: jasmine.SpyObj<Router>;
+  let toastCtrlSpy: jasmine.SpyObj<ToastController>;
+  let toastSpy;
+
+  beforeEach(waitForAsync(() => {
+    authServiceSpy = jasmine.createSpyObj('AuthenticationService', ['login']);
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    toastCtrlSpy = jasmine.createSpyObj('ToastController', ['create']);
+    toastSpy = jasmine.createSpyObj('Toast', ['present']);
+
+    TestBed.configureTestingModule({
+      declarations: [ LoginPage ],
+      imports: [ ReactiveFormsModule ],
+      providers: [
+        FormBuilder,
+        { provide: AuthenticationService, useValue: authServiceSpy },
+        { provide: Router, useValue: routerSpy },
+        { provide: ToastController, useValue: toastCtrlSpy }
+      ]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    toastCtrlSpy.create.and.resolveTo(toastSpy);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Navega correctamente', async () => {
+    authServiceSpy.login.and.returnValue(of(true));
+    await component.login();
+    expect(toastSpy.present).toHaveBeenCalled();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/pageinitier']);
+  });
+
+  it('Entro ala sistema', async () => {
+    authServiceSpy.login.and.returnValue(of(false));
+    await component.login();
+    expect(toastSpy.present).toHaveBeenCalled();
+    expect(toastCtrlSpy.create).toHaveBeenCalledWith(jasmine.objectContaining({
+      message: 'Credenciales incorrectas',
+      duration: 2000
+    }));
+    expect(routerSpy.navigate).not.toHaveBeenCalled();
+  });
+
+  it('login errors', async () => {
+    authServiceSpy.login.and.returnValue(throwError(() => new Error('login failed')));
+    await component.login();
+    expect(toastSpy.present).toHaveBeenCalled();
+    expect(toastCtrlSpy.create).toHaveBeenCalledWith(jasmine.objectContaining({
+      message: 'Error en el sistema de inicio de sesión',
+      duration: 2000
+    }));
+    expect(routerSpy.navigate).not.toHaveBeenCalled();
   });
 });
--- src/app/register/register.page.spec.ts
+++ src/app/register/register.page.spec.ts
@@ -1,17 +1,73 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RegisterPage } from './register.page';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthenticationService } from '../services/authentication.service';
+import { ToastController } from '@ionic/angular';
+import { of, throwError } from 'rxjs';
 
 describe('RegisterPage', () => {
   let component: RegisterPage;
   let fixture: ComponentFixture<RegisterPage>;
+  let authService: jasmine.SpyObj<AuthenticationService>;
+  let toastController: jasmine.SpyObj<ToastController>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    const authServiceSpy = jasmine.createSpyObj('AuthenticationService', ['register']);
+    const toastControllerSpy = jasmine.createSpyObj('ToastController', ['create']);
+
+    await TestBed.configureTestingModule({
+      declarations: [ RegisterPage ],
+      imports: [ ReactiveFormsModule, RouterTestingModule ],
+      providers: [
+        FormBuilder,
+        { provide: AuthenticationService, useValue: authServiceSpy },
+        { provide: ToastController, useValue: toastControllerSpy }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RegisterPage);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthenticationService) as jasmine.SpyObj<AuthenticationService>;
+    toastController = TestBed.inject(ToastController) as jasmine.SpyObj<ToastController>;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('crea el formulario con tres campos', () => {
+    expect(component.registerForm.contains('username')).toBeTrue();
+    expect(component.registerForm.contains('password')).toBeTrue();
+    expect(component.registerForm.contains('email')).toBeTrue();
+  });
+
+  it('username debe ser obligatorio', () => {
+    const control = component.registerForm.get('username');
+    control.setValue('');
+    expect(control.valid).toBeFalse();
+  });
+
+  it('se debe registrar un usuario exitosamente', async () => {
+    authService.register.and.returnValue(of({}));
+    toastController.create.and.resolveTo({
+      present: jasmine.createSpy()
+    } as any);  
+
+    component.register();
+
+    expect(authService.register).toHaveBeenCalledWith('user', 'pass');
+    expect(toastController.create).toHaveBeenCalledTimes(1);
+    expect(toastController.create.calls.mostRecent().args[0].message).toEqual('Registro exitoso');
+  });
+
+  it('maneja errores en el registro', async () => {
+    authService.register.and.returnValue(throwError(() => new Error('Error en el registro')));
+    toastController.create.and.resolveTo({
+      present: jasmine.createSpy()
+    } as any); 
+
+    component.register();
+
+    expect(authService.register).toHaveBeenCalledWith('user', 'pass');
+    expect(toastController.create).toHaveBeenCalledTimes(1);
+    expect(toastController.create.calls.mostRecent().args[0].message).toEqual('Error en el registro');
   });
 });
